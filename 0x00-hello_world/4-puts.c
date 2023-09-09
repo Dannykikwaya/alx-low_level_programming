44ab449f07824ecd27f363c68a394f9e9eec56ed
Danny kikwaya <landrykasindidanny@gmail.com>
2023-09-09 15:18:29 -0700

    puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * puts: puts function
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzlle");
+	return (0);
+}
