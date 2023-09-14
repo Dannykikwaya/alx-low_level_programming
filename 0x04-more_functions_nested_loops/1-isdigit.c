db805660c60aba81a95c8bce6bc080808c500969
Danny kikwaya <landrykasindidanny@gmail.com>
2023-09-14 12:50:19 -0700

    Verifier digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit: Function for upper case
+ * @c: parameter
+ * return: Always 0hgg is succes
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
