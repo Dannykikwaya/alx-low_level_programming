26807bd231abd8ff6c4baf67f2e4a845940e78ba
Danny kikwaya <landrykasindidanny@gmail.com>
2023-09-14 12:56:08 -0700

    mulpicatoln

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - function
+ *
+ * @a: variable to tested
+ *
+ * @b: variable to tested
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int result;
+	
+	result = a * b;
+	return (result);
+}
