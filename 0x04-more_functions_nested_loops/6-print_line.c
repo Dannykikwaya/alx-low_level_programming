b2eb5ad572fec1be91a80cfc266112c93322ec6a
Danny kikwaya <landrykasindidanny@gmail.com>
2023-09-15 08:19:10 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - function
+ * @n: Number of tpimes _ can be used
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
