e57d13e32cbe0bfa3a3f895aa8128426d812b87c
Danny kikwaya <landrykasindidanny@gmail.com>
2023-09-16 03:26:50 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry functio
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 != 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+		}
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		if (i % 3 && i % 5 && i % 15 != 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
